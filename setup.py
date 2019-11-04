098bf4160d82212b7e2a356d537e9514ee8382b3
Souvik Haldar <souvik@Souviks-MacBook-Pro.local>
2019-11-04 11:59:24 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+mport setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-dsa", 
+    version="0.0.1",
+    author="Souvik Haldar",
+    author_email="souvikhaldar32@gmail.com",
+    description="All abstract data types and algorithms implemented in Python 3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
